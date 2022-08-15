82f1e7b3bbf60211cb7f2cb2acc6ade50b9eec34
yuda <100904133+judyzero@users.noreply.github.com>
2022-08-15 22:55:25 +0900

    게시판

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var app = express();
+
+// DB setting
+mongoose.connect(process.env.MONGO_URL
+  );
+var db = mongoose.connection;
+db.once('open', function(){
+  console.log('DB connected');
+});
+db.on('error', function(err){
+  console.log('DB ERROR : ', err);
+});
+
+// Other settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+
+app.engine('html', require('ejs').renderFile);  //html로 렌더링
+// Routes
+app.use('/', require('./routes/home'));
+app.use('/posts', require('./routes/posts'));
+// Port setting
+var port = 3000;
+app.listen(port, function(){
+  console.log('server on! http://localhost:'+port);
+});
