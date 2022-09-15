75379f4424d3c4dbe762f70c5d5bf3fd2c79a828
Yun Min Woo <77202633+ymw0407@users.noreply.github.com>
2022-09-15 12:42:52 +0900

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.engine('html', require('ejs').renderFile);  //html로 렌더링
 app.use('/', require('./routes/post'));
 
 // Port setting
-var port = 80;
+var port = 8080;
 app.listen(port, function(){
   console.log('server on! http://localhost:'+port);
   
